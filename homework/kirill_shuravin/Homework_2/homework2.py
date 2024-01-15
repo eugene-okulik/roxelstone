4c6f60dc15834c11b3a9cf0e244d7ee2a6ed74de
roxelstone <roxelstone@gmail.com>
2024-01-15 13:24:31 +0300

    commit homework4here!

--- homework/kirill_shuravin/Homework_2/homework2.py
+++ homework/kirill_shuravin/Homework_2/homework2.py
@@ -8,7 +8,7 @@ my_dict['list'].pop(1)
 # print(my_dict['list']) ## Для Проверок!
 
 my_dict['dict'] = {'time': '22', 'size': 'XS', 'trueOrNot': False,
-    'yesOrNot': None, 'age': '22'}
+'yesOrNot': None, 'age': '22'}
 my_dict['dict'][('i am a tuple',)] = 'Inoske'
 my_dict['dict'].pop('time')
 # print(my_dict['dict']) ## Для Проверок!
