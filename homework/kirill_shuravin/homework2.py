bfa5c50c296da35417e0c396a0e2232cbff93624
roxelstone <roxelstone@gmail.com>
2024-01-15 22:49:00 +0300

    homework4Here

--- homework/kirill_shuravin/homework2.py
+++ homework/kirill_shuravin/homework2.py
@@ -1,15 +1,14 @@
 my_dict = {'tuple': '', 'list': '',
-        'dict': '', 'set': ''}
+           'dict': '', 'set': ''}
 my_dict['tuple'] = (12, 'massive', False, None, '34')
-# print(my_dict['tuple'][ - 1]) ## Для Проверок!
+print(my_dict['tuple'][-1])
 
 my_dict['list'] = ['Donut', 'Soup', 'Car', 'Bread', 'Tomatoes']
 my_dict['list'].append('Watermelon')
 my_dict['list'].pop(1)
 # print(my_dict['list']) ## Для Проверок!
 
-my_dict['dict'] = {'time': '22', 'size': 'XS', 'trueOrNot': False,
-    'yesOrNot': None, 'age': '22'}
+my_dict['dict'] = {'time': '22', 'size': 'XS', 'trueOrNot': False, 'yesOrNot': None, 'age': '22'}
 my_dict['dict'][('i am a tuple',)] = 'Inoske'
 my_dict['dict'].pop('time')
 # print(my_dict['dict']) ## Для Проверок!
@@ -21,7 +20,6 @@ my_dict['set'].remove('1')
 
 
 print(my_dict)
-
 # Проверка типов
 # print(type(my_dict)['tuple'])
 # print(type(my_dict)['list'])
